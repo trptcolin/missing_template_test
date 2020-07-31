b9b367c19b2098c51215cb8fe43704ed8d20316b
Colin Jones <trptcolin@gmail.com>
2020-07-31 16:52:31 -0500

    Reproduce race condition repeatably

--- spec/views/images/index.html.erb_spec.rb
+++ spec/views/images/index.html.erb_spec.rb
@@ -4,15 +4,24 @@ RSpec.describe "images/index", type: :view do
   before(:each) do
     assign(:images, [
       Image.create!(
-        name: "Name"
+        name: "Name",
+        original: Rack::Test::UploadedFile.new(File.join("spec", "images", "halp-cat.jpg"))
       ),
       Image.create!(
-        name: "Name"
-      )
+        name: "Name",
+        original: Rack::Test::UploadedFile.new(File.join("spec", "images", "halp-cat.jpg"))
+      ),
     ])
   end
 
   it "renders a list of images" do
+    sleep 1
+    render
+    assert_select "tr>td", text: "Name".to_s, count: 2
+  end
+
+  it "renders a list of images again" do
+    sleep 1
     render
     assert_select "tr>td", text: "Name".to_s, count: 2
   end
